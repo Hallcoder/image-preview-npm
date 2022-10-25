98c2dc93a990aabfe5f382920c4116f71fa759bc
Hallcoder <hallcoder25@gmail.com>
2022-10-25 14:46:40 +0200

    resolve prpmise

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-export default class Image{
-    files;
-    file;
-    baseImage;
-    constructor(files){
+export default class Image {
+  files;
+  file;
+  baseImage;
+  constructor(files){
     this.files = files;
-    }
-    getBase64Image(){
-            this.file = this.files[0];
-            const reader = new FileReader();
-            reader.readAsDataURL(this.file);
-            reader.onloadend = () => {
-              this.baseImage =  reader.result;
-            };
-    }
+  }
+  getBase64Image = () => {
+    return new Promise(resolve => {
+      this.file = this.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(this.file);
+      reader.onloadend = () => {
+       this.baseImage = reader.result;
+       resolve(this.baseImage);
+      };
+    })
+  }
 }
